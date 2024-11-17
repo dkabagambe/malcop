af41e8241f853c5230a11a8ace0eee6870a19076
daniel Kabagambe <danielkabagambe@gmail.com>
2024-11-17 21:51:38 +0300

    <Card.Text>$ {product.price}</Card.Text>

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -36,7 +36,8 @@ function Product(props) {
           <Card.Title>{product.name}</Card.Title>
         </Link>
         {/* <Rating rating={product.rating} numReviews={product.numReviews} /> */}
-        <Card.Text>Ugx {product.price}</Card.Text>
+        {/* <Card.Text>$ {product.price}</Card.Text> */}
+        <Card.Text>$$$$ </Card.Text>
         {product.countInStock === 0 ? (
           <Button variant="light" disabled>
             Out of stock
